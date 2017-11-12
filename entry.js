f64dbd851f8b55fb93271bdf2e9a1b577b91c7d3
jcmhua <jcmhua@gmail.com>
2017-11-11 16:55:56 -0800

    added hot module replacement

--- entry.js
+++ entry.js
@@ -1,7 +1,21 @@
-//Hot Module Replacement API
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { AppContainer } from 'react-hot-loader'
+import { App } from './Components/App'
+
+const render = Component => {
+  ReactDOM.render(
+    <AppContainer>
+      <Component />
+    </AppContainer>,
+    document.getElementById('root'),
+  )
+}
+
+
+render(App)
+
+//Hot Module Replacement API / React-hot-loader API
 if (module.hot) {
-   module.hot.accept('./App.js', function() {
-     console.log('Accepting the updated printMe module!');
-     printMe();
-   })
+   module.hot.accept('./Components/App.js', () => { render(App) })
  }
